47a8cd2e3ea20cd03f831d3f30a71119aa3f6348
Matthew Ellis <mbellistx@gmail.com>
2021-03-12 14:32:20 -0600

    CheckoutForm tests done

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,27 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
+
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  render(<CheckoutForm/>)
+  // screen.getByText('CheckoutForm') Test Fails
+  screen.getByText('Checkout Form')
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+  render(<CheckoutForm/>)
+  const checkout = screen.getByText('Checkout')
+  const success = () => screen.queryByTestId('successMessage')
+
+
+  // expect(success()).toBeInTheDocument() Test fails
+  expect(success()).not.toBeInTheDocument()
+
+  userEvent.click(checkout)
+
+  expect(success()).toBeInTheDocument()
+});
