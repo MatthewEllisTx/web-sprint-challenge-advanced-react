515a70645a8d5a1a82fb66a693850063d0a35e78
Matthew Ellis <mbellistx@gmail.com>
2021-03-12 15:28:56 -0600

    test input checkout form

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -12,7 +12,7 @@ test("form header renders", () => {
   screen.getByText('Checkout Form')
 });
 
-test("form shows success message on submit with form details", () => {
+test("form shows success message on submit", () => {
   render(<CheckoutForm/>)
   const checkout = screen.getByText('Checkout')
   const success = () => screen.queryByTestId('successMessage')
@@ -24,4 +24,36 @@ test("form shows success message on submit with form details", () => {
   userEvent.click(checkout)
 
   expect(success()).toBeInTheDocument()
+});
+
+test("form shows success message on submit with form details", async () => {
+  render(<CheckoutForm/>)
+
+  const firstName = screen.getByLabelText('First Name:')
+  const lastName = screen.getByLabelText('Last Name:')
+  const address = screen.getByLabelText('Address:')
+  const city = screen.getByLabelText('City:')
+  const state = screen.getByLabelText('State:')
+  const zip = screen.getByLabelText('Zip:')
+  const checkout = screen.getByText('Checkout')
+  const success = () => screen.queryByTestId('successMessage')
+
+
+  // expect(success()).toBeInTheDocument() Test fails
+  expect(success()).not.toBeInTheDocument()
+
+  userEvent.type(zip, 'zip')
+  userEvent.type(state, 'state')
+  userEvent.type(city, 'city')
+  userEvent.type(address, 'address')
+  userEvent.type(lastName, 'lastName')
+  userEvent.type(firstName, 'firstName')
+
+  userEvent.click(checkout)
+
+  expect(success()).toBeInTheDocument()
+
+  expect(await screen.findByText('firstName lastName')).toBeVisible()
+  expect(await screen.findByText('address')).toBeVisible()
+  expect(await screen.findByText('city, state zip')).toBeVisible()
 });
